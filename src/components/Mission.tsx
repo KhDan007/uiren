f17fc672cf09983a7fdcd07fd004cab81920604c
KhDan007 <kh.dan007@gmail.com>
2024-03-29 22:18:47 +0600

    finish mission block

--- src/components/Mission.tsx
+++ src/components/Mission.tsx
@@ -1,12 +1,24 @@
 import image1 from "../img/mission/1.png";
+import React, { useState, useEffect } from 'react';
 
 export const Mission = () => {
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    const element = document.querySelector('.mission__circle') as HTMLElement;
+    setWidth(element?.offsetWidth);
+
+    window.addEventListener('resize', () => {
+      setWidth(element?.offsetWidth);
+    });
+  }, []);
+
   return (
     <section className="bg-gradient-to-b from-green3 to-green4 px-3">
       <div className="container md:flex mx-auto items-center">
-        <div className="mission__column md:w-1/2">
-          <img src={image1} alt="no image" />
-          <div className="mission__circle h-10 w-3/5 translate-x-1/4 bg-blue1"></div>
+        <div className="mission__column md:w-1/2 relative">
+          <img className="relative z-10" src={image1} alt="no image" />
+          <div className="mission__circle h-auto w-3/5 translate-x-1/4 bg-blue1 absolute top-1/4 rounded-full box-shadow left-0 z-0" style={{ height: width }}></div>
         </div>
         <div className="mission__column py-10 md:py-20 md:w-1/2">
           <div className="mission__elem bg-green5 py-6 lg:py-16 px-10 rounded-3xl text-white text-shadow font-PublicSans mb-8 lg:mb-16">
@@ -34,3 +46,5 @@ export const Mission = () => {
     </section>
   );
 };
+
+export default Mission;
