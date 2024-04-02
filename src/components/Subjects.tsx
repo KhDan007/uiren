0bf90c9c360e7cf414d1da194027bc81350fbf27
KhDan007 <kh.dan007@gmail.com>
2024-04-02 19:48:59 +0600

    fix subjects block

--- src/components/Subjects.tsx
+++ src/components/Subjects.tsx
@@ -10,8 +10,8 @@ import icon9 from "../img/icons/subjects/9.svg";
 
 export const Subjects = () => {
   return (
-    <section className="bg-gray py-24">
-      <div className="container mx-auto">
+    <section className="bg-gray py-12 md:py-16 lg:py-24">
+      <div className="mx-auto px-4">
         <h2 className="subjects__title text-shadow font-semibold mb-4 text-5xl">
           Наши предметы
         </h2>
@@ -19,61 +19,61 @@ export const Subjects = () => {
           Вы можете ознакомится с предметами, которые входят в наши курсы
           обучения ...
         </div>
-        <div className="flex justify-between">
-          <div className="subjects__column w-1/4">
-            <div className="subjects__elem mb-8 flex bg-white  items-center font-PublicSans py-4 px-6 rounded-2xl justify-center">
+        <div className="flex flex-col md:flex-row md:justify-between">
+          <div className="subjects__column flex flex-col justify-between md:mr-4">
+            <div className="subjects__elem mb-4 md:mb-8 flex bg-white items-center font-PublicSans py-2 md:py-4 px-6 rounded-2xl justify-center">
               <div className="subjects__subcolumn mr-4">
                 <img src={icon1} alt="" />
               </div>
               <div className="subjects__subcolumn text-xl">Английский язык</div>
             </div>
-            <div className="subjects__elem mb-8 flex bg-white items-center font-PublicSans py-4 px-6 rounded-2xl justify-center">
+            <div className="subjects__elem mb-4 md:mb-8 flex bg-white items-center font-PublicSans py-2 md:py-4 px-6 rounded-2xl justify-center">
               <div className="subjects__subcolumn mr-4">
                 <img src={icon2} alt="" />
               </div>
               <div className="subjects__subcolumn text-xl">Математика</div>
             </div>
-            <div className="subjects__elem flex bg-white items-center font-PublicSans py-4 px-6 rounded-2xl justify-center">
+            <div className="subjects__elem mb-4 md:m-0 flex bg-white items-center font-PublicSans py-2 md:py-4 px-6 rounded-2xl justify-center">
               <div className="subjects__subcolumn mr-4">
                 <img src={icon3} alt="" />
               </div>
               <div className="subjects__subcolumn text-xl">История</div>
             </div>
           </div>
-          <div className="subjects__column w-1/4">
-            <div className="subjects__elem mb-8 flex bg-white  items-center font-PublicSans py-4 px-6 rounded-2xl justify-center">
+          <div className="subjects__column flex flex-col justify-between mr-4">
+            <div className="subjects__elem mb-4 md:mb-8 flex bg-white  items-center font-PublicSans py-2 md:py-4 px-6 rounded-2xl justify-center">
               <div className="subjects__subcolumn mr-4">
                 <img src={icon4} alt="" />
               </div>
               <div className="subjects__subcolumn text-xl">Казахский язык</div>
             </div>
-            <div className="subjects__elem mb-8 flex bg-white items-center font-PublicSans py-4 px-6 rounded-2xl justify-center">
+            <div className="subjects__elem mb-4 md:mb-8 flex bg-white items-center font-PublicSans py-2 md:py-4 px-6 rounded-2xl justify-center">
               <div className="subjects__subcolumn mr-4">
                 <img src={icon5} alt="" />
               </div>
               <div className="subjects__subcolumn text-xl">Шахматы</div>
             </div>
-            <div className="subjects__elem flex bg-white items-center font-PublicSans py-4 px-6 rounded-2xl justify-center">
+            <div className="subjects__elem mb-4 md:mb-0 flex bg-white items-center font-PublicSans py-2 md:py-4 px-6 rounded-2xl justify-center">
               <div className="subjects__subcolumn mr-4">
                 <img src={icon6} alt="" />
               </div>
               <div className="subjects__subcolumn text-xl">Рисование</div>
             </div>
           </div>
-          <div className="subjects__column w-1/4">
-            <div className="subjects__elem mb-8 flex bg-white items-center font-PublicSans py-4 px-6 rounded-2xl justify-center">
+          <div className="subjects__column flex flex-col justify-between">
+            <div className="subjects__elem mb-4 md:mb-8 flex bg-white items-center font-PublicSans py-2 md:py-4 px-6 rounded-2xl justify-center">
               <div className="subjects__subcolumn mr-4">
                 <img src={icon7} alt="" />
               </div>
               <div className="subjects__subcolumn text-xl">Русский язык</div>
             </div>
-            <div className="subjects__elem mb-8 flex bg-white  items-center font-PublicSans py-4 px-6 rounded-2xl justify-center">
+            <div className="subjects__elem mb-4 md:mb-8 flex bg-white  items-center font-PublicSans py-2 md:py-4 px-6 rounded-2xl justify-center">
               <div className="subjects__subcolumn mr-4">
                 <img src={icon8} alt="" />
               </div>
               <div className="subjects__subcolumn font-normal text-xl">ICT</div>
             </div>
-            <div className="subjects__elem flex bg-white items-center font-PublicSans py-4 px-6 rounded-2xl justify-center">
+            <div className="subjects__elem mb-4 md:mb-0 flex bg-white items-center font-PublicSans py-2 md:py-4 px-6 rounded-2xl justify-center">
               <div className="subjects__subcolumn mr-4">
                 <img src={icon9} alt="" />
               </div>
